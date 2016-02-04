79277690e3c7873836b787dfb349967506975886
Mick Timony <timony@hkl.hms.harvard.edu>
2016-02-04 15:41:01 -0500

    copy of code used on server

---
+++ registration/backends/default/urls.py
@@ -0,0 +1,60 @@
+"""
+URLconf for registration and activation, using django-registration's
+default backend.
+
+If the default behavior of these views is acceptable to you, simply
+use a line like this in your root URLconf to set up the default URLs
+for registration::
+
+    (r'^accounts/', include('registration.backends.default.urls')),
+
+This will also automatically set up the views in
+``django.contrib.auth`` at sensible default locations.
+
+If you'd like to customize the behavior (e.g., by passing extra
+arguments to the various views) or split up the URLs, feel free to set
+up your own URL patterns for these views instead.
+
+"""
+
+
+from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+
+from registration.views import activate, resend_by_username, resend_by_email
+
+urlpatterns = patterns('',
+                       url(r'^resend_activation_key/(?P<username>\w+)/$',
+                           resend_by_username,
+                           {'backend': 'registration.backends.default.DefaultBackend'}, 
+                           name='registration_resend_username'),
+                       url(r'^resend_activation_key/(?P<email>\w+@\w+)/$',
+                           resend_by_email,
+                           {'backend': 'registration.backends.default.DefaultBackend'},
+                           name='registration_resend_email'),
+                       url(r'^activate/complete/$',
+                           direct_to_template,
+                           {'template': 'registration/activation_complete.html'},
+                           name='registration_activation_complete'),
+                       # Activation keys get matched by \w+ instead of the more specific
+                       # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
+                       # that way it can return a sensible "invalid key" message instead of a
+                       # confusing 404.
+                       url(r'^activate/(?P<activation_key>\w+)/$',
+                           activate,
+                           {'backend': 'registration.backends.default.DefaultBackend'},
+                           name='registration_activate'),
+                       url(r'^register/$',
+                           register,
+                           {'backend': 'registration.backends.default.DefaultBackend'},
+                           name='registration_register'),
+                       url(r'^register/complete/$',
+                           direct_to_template,
+                           {'template': 'registration/registration_complete.html'},
+                           name='registration_complete'),
+                       url(r'^register/closed/$',
+                           direct_to_template,
+                           {'template': 'registration/registration_closed.html'},
+                           name='registration_disallowed'),
+                       (r'', include('registration.auth_urls')),
+                       )
